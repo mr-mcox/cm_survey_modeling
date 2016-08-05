2478d2478e83dd376db4f6f484fc1a02d87c2871
Matthew Cox <matthew.cox@teachforamerica.org>
2016-08-05 09:55:17 -0500

    Adding cohort computations

--- surveyformat.py
+++ surveyformat.py
@@ -1,9 +1,25 @@
+import pandas as pd
+
+
 def add_dimensions(df):
     out = remove_blank_corps(df)
     return out
 
+
 def melt(df):
     return df
 
+
 def remove_blank_corps(df):
-    return df[df.Corps.notnull()]°
+    return df[df.Corps.notnull()]
+
+
+def add_cohort(df):
+    out = df.copy()
+
+    year_digits = out.survey_code.str.extract('^(\d\d)', expand=False)
+    cohort_str = '20' + year_digits
+
+    out['cohort'] = pd.to_numeric(cohort_str)
+    out.ix[out.Corps=='2nd year','cohort'] = out.ix[out.Corps=='2nd year','cohort'] - 1
+    return out
--- tests/test_dimension_steps.py
+++ tests/test_dimension_steps.py
@@ -1,7 +1,40 @@
 import pandas as pd
-from surveyformat import remove_blank_corps
+import pytest
+from surveyformat import remove_blank_corps, add_cohort
+
 
 def test_remove_rows_without_corps():
-    df = pd.DataFrame({'Corps':['1st year',None,'2nd year']})
+    df = pd.DataFrame({'Corps': ['1st year', None, '2nd year']})
     out = remove_blank_corps(df)
     assert out.Corps.isnull().sum() == 0
+
+
+@pytest.fixture
+def cohort_input():
+    cols = ['Corps', 'survey_code']
+    recs = [('1st year', '1516EYS'),
+            ('1st year', '1415EYS'),
+            ('2nd year', '1415EYS')]
+    df = pd.DataFrame.from_records(recs, columns=cols)
+    return df
+
+def test_add_cohort_case_1(cohort_input):
+
+    out = add_cohort(cohort_input)
+
+    assert (out.ix[(out.Corps == '1st year') & (
+        out.survey_code == '1516EYS'), 'cohort'] == 2015).all()
+
+def test_add_cohort_case_2(cohort_input):
+
+    out = add_cohort(cohort_input)
+
+    assert (out.ix[(out.Corps == '1st year') & (
+        out.survey_code == '1415EYS'), 'cohort'] == 2014).all()
+
+def test_add_cohort_case_3(cohort_input):
+
+    out = add_cohort(cohort_input)
+
+    assert (out.ix[(out.Corps == '2nd year') & (
+        out.survey_code == '1415EYS'), 'cohort'] == 2013).all()
