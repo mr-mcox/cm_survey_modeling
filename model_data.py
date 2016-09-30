dac4346a26313d73787780ac6e78ab877d580ca9
Matthew Cox <matthew.cox@teachforamerica.org>
2016-09-30 15:16:29 -0500

    Gets proportion for a given cut

--- model_data.py
+++ model_data.py
@@ -1,4 +1,7 @@
 from surveyformat import add_survey_seq as format_survey_seq
+import pandas as pd
+import itertools
+
 
 class ModelData(object):
 
@@ -6,6 +9,7 @@ class ModelData(object):
 
     def __init__(self, df=None):
         self.df = df
+        self._prop_calcs = dict()
 
     def assign_previous_response(self):
         if 'survey_seq' not in self.df.columns:
@@ -14,7 +18,7 @@ class ModelData(object):
         p_df = self.df.set_index(['person_id', 'next_survey_seq'])
         df['prev_response'] = p_df.response
         df.reset_index(inplace=True)
-        print(df)
+        self.df = df
         return df
 
     def add_survey_seq(self):
@@ -25,3 +29,54 @@ class ModelData(object):
             self.df = self.add_survey_seq()
         self.df['next_survey_seq'] = self.df.survey_seq + 1
         return self.df
+
+    def count_by_prev_response(self, cut_cols=None):
+        if 'survey_seq' not in self.df.columns:
+            self.assign_previous_response()
+
+        df = self.df
+
+        cols = list()
+        if cut_cols is None:
+            cols = [c for c in df.columns if c != 'person_id']
+        else:
+            cols = cut_cols + ['response', 'prev_response']
+
+        size_s = df.groupby(cols).size()
+
+        out = pd.DataFrame({'num': size_s}, index=size_s.index).reset_index()
+        return out
+
+    def calculate_proportions(self, input_df):
+        df = input_df.copy()
+        cols_for_groups = [
+            c for c in df.columns if c not in ['num', 'response']]
+        sum_s = df.groupby(cols_for_groups).sum()['num']
+        df.set_index(cols_for_groups,  inplace=True)
+        df['total_num'] = sum_s
+        df['percent'] = df.num / df.total_num
+        return df.reset_index()
+
+    def proportion_for_cut(self, prev_response, unit, groups=dict()):
+        cut_cols = [k for k in groups.keys()] + [unit]
+        cut_cols_tup = tuple(cut_cols)
+        if cut_cols_tup not in self._prop_calcs:
+            counts = self.count_by_prev_response(cut_cols=cut_cols)
+            calc_props = self.calculate_proportions(counts)
+            self._prop_calcs[cut_cols_tup] = calc_props
+        df = self._prop_calcs[cut_cols_tup]
+
+        full_mask = [True] * len(df)
+        for key, value in groups.items():
+            full_mask = full_mask & (df[key] == value)
+        df = df.ix[full_mask]
+        df.set_index('response', inplace=True)
+        df_pr = df.ix[df.prev_response == prev_response, 'percent']
+        out_list = list()
+        for i in range(1, 8):
+            val = 0
+            if i in df_pr.index:
+                val = df_pr.get(i)
+            out_list.append(val)
+
+        return out_list
--- tests/test_data_for_model.py
+++ tests/test_data_for_model.py
@@ -7,7 +7,7 @@ from model_data import ModelData
 def md_with_dataframe():
     cols = 'person_id   question_code   survey_code response Corps'.split()
     recs = [(3554131, 'CSI1', '1415MYS', 5, '1st year'),
-            (3490339, 'CSI1', '1415MYS', 6, '1st year'),
+            (3490339, 'CSI1', '1415MYS', 3, '1st year'),
             (3557769, 'CSI1', '1415MYS', 3, '1st year'),
             (3013575, 'CSI1', '1415MYS', 5, '1st year'),
             (3554131, 'CSI1', '1415EYS', 6, '1st year'),
@@ -19,12 +19,35 @@ def md_with_dataframe():
     return md
 
 
+@pytest.fixture
+def md_with_dataframe_region(md_with_dataframe):
+    md = md_with_dataframe
+    cm_region = pd.DataFrame.from_records([
+        (3554131, 'Atlanta'),
+        (3490339, 'Atlanta'),
+        (3557769, 'Chicago'),
+        (3013575, 'Chicago'),
+    ], columns=['person_id', 'Region'])
+    md.df = pd.merge(md.df, cm_region)
+    return md
+
+
 def test_process_functional(md_with_dataframe):
     # Given enerate proportions for a cut
     md = md_with_dataframe
     res = md.assign_previous_response()
     assert res.ix[(res.person_id == 3554131) & (
         res.survey_code == '1415EYS'), 'prev_response'].iat[0] == 5
+
+    # Group responses
+    gres = md.count_by_prev_response()
+    assert gres.ix[
+        (gres.prev_response == 3) & (gres.response == 6), 'num'].iat[0] == 2
+
+    # Calculate responsses
+    res = md.calculate_proportions(gres)
+    assert res.ix[
+        (res.prev_response == 5) & (res.response == 6), 'percent'].iat[0] == 0.5
 
 
 def test_assign_previous_response(md_with_dataframe):
@@ -42,8 +65,41 @@ def test_add_survey_seq(md_with_dataframe):
     assert (res.ix[res.survey_code == '1415EYS', 'survey_seq'] >
             res.ix[res.survey_code == '1415MYS', 'survey_seq']).all()
 
+
 def test_add_next_survey_seq(md_with_dataframe):
     md = md_with_dataframe
     res = md.add_next_survey_seq()
-    assert (res.survey_seq+1 == res.next_survey_seq).all()
+    assert (res.survey_seq + 1 == res.next_survey_seq).all()
 
+
+def test_count_by_prev_response(md_with_dataframe):
+    md = md_with_dataframe
+    res = md.count_by_prev_response()
+    assert res.ix[
+        (res.prev_response == 3) & (res.response == 6), 'num'].iat[0] == 2
+
+
+def test_count_by_prev_response_specify_cut(md_with_dataframe_region):
+    md = md_with_dataframe_region
+    res = md.count_by_prev_response(cut_cols=['survey_code'])
+    assert res.ix[
+        (res.prev_response == 3) & (res.response == 6), 'num'].iat[0] == 2
+
+
+def test_proportion_for_cut(md_with_dataframe):
+    md = md_with_dataframe
+    res = md.proportion_for_cut(prev_response=5, unit='survey_code')
+    assert res == [0, 0, 0, 0, 0.5, 0.5, 0]
+
+
+def test_proportion_for_cut_v2(md_with_dataframe_region):
+    md = md_with_dataframe_region
+    res = md.proportion_for_cut(prev_response=5, unit='survey_code')
+    assert res == [0, 0, 0, 0, 0.5, 0.5, 0]
+
+
+def test_proportion_for_cut_v3(md_with_dataframe_region):
+    md = md_with_dataframe_region
+    res = md.proportion_for_cut(
+        prev_response=5, unit='survey_code', groups={'Region': 'Atlanta'})
+    assert res == [0, 0, 0, 0, 0, 1, 0]
