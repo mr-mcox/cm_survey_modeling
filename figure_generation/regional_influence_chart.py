d4e01dea039b00e8ec51728027b48c763d40fa81
Matthew Cox <matthew.cox@teachforamerica.org>
2016-11-14 21:16:06 -0600

    Regional bar plot created

--- figure_generation/regional_influence_chart.py
+++ figure_generation/regional_influence_chart.py
@@ -3,6 +3,8 @@ import json
 from os import path
 from helpers.examine_trace import label_trace
 import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 with pd.HDFStore(path.join('..', 'inputs', 'responses.h5')) as store:
     resp = pd.DataFrame()
@@ -36,9 +38,20 @@ regs = pd.concat(regionals)
     
 reg_csi = regs.groupby(['Region','i'])['net'].mean().reset_index()
 
+reg_csi_qs = reg_csi.groupby('Region')['net'].quantile([0.05, 0.25, 0.75, 0.95]).unstack(level=-1)
+reg_csi_qs.sort_values(by=0.05, inplace=True)
+reg_csi_qs['b_width_l'] = reg_csi_qs[0.95] - reg_csi_qs[0.05]
+reg_csi_qs['b_width_s'] = reg_csi_qs[0.75] - reg_csi_qs[0.25]
 
-reg_csi_qs = reg_csi.groupby('Region')['net'].quantile(0.05)
-reg_rank = reg_csi_qs.sort_values(ascending=False).index
+fig, ax = plt.subplots(figsize=(10, 15))
+fig.suptitle('CSI at 1617F8W by Region')
+plt.barh(bottom=np.arange(len(reg_csi_qs))-0.5, width=reg_csi_qs.b_width_l, left=reg_csi_qs[0.05], alpha=0.4, label='90% credible CSI')
+plt.barh(bottom=np.arange(len(reg_csi_qs))-0.5, width=reg_csi_qs.b_width_s, left=reg_csi_qs[0.25], alpha=0.2)
+plt.ylim(ymin=-0.5, ymax=len(reg_csi_qs))
+ax.yaxis.set_ticks(range(len(reg_csi_qs)))
+ax.set_yticklabels(reg_csi_qs.index)
+xvals = ax.get_yticks()
+ax.set_xticklabels(['{:3.0f}%'.format(x*10) for x in xvals])
+ax.legend(loc='lower right')
+plt.show()
 
-g = sns.FacetGrid(reg_csi, row='Region', row_order=reg_rank, size=1.7, aspect=4)
-g.map(sns.distplot, 'net', hist=False)
