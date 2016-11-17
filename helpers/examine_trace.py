f2d67dca64a28c776ab1ce7072342aadf12e9943
Matthew Cox <matthew.cox@teachforamerica.org>
2016-11-17 08:49:07 -0600

    Adding ability to create multiple nets

--- helpers/examine_trace.py
+++ helpers/examine_trace.py
@@ -2,35 +2,61 @@ import pandas as pd
 
 
 def label_trace(trace, heads):
-    thresh_cols = trace.columns.str.contains('thresh__\d')
-    numbered_cols = trace.columns.str.contains('__\d')
-    numbered_cols = numbered_cols & (~thresh_cols)
+    trace['i'] = range(len(trace))
+    mlt = pd.melt(trace, id_vars='i')
 
-    overall_df = trace.loc[:, ~numbered_cols]
-    overall_df['i'] = range(len(overall_df))
+    # Reformat thresh cols
+    thresh_num = mlt.variable.str.extract('thresh__(\d)', expand=False)
+    mlt.ix[thresh_num.notnull(), 'variable'] = 'thresh' + thresh_num
 
-    numbered_df = trace.loc[:, numbered_cols]
-    numbered_df['i'] = range(len(numbered_df))
-    mlt = pd.melt(numbered_df, id_vars='i')
-    mlt['idx_num'] = pd.to_numeric(mlt.variable.str.extract('__(\d+)'))
-    mlt[heads[0]['name']] = mlt.idx_num.map(lambda x: heads[0]['values'][x])
-    mlt['var_name'] = mlt.variable.str.extract('([\w_]+)__')
+    # Reformat ps
+    ps_base = mlt.variable.str.extract('(.*ps)_', expand=False)
+    ps_num = mlt.variable.str.extract('.*ps_.*(\d)$', expand=False)
+    ps_rest = mlt.variable.str.extract('.*ps(__.*)_\d$', expand=False)
+    ps_rest.fillna('', inplace=True)
 
-    # Rework ps
-    mlt_ps = mlt.ix[mlt.var_name == 'ps']
-    mlt_ps['var_name'] = mlt_ps['var_name'] + \
-        mlt_ps.variable.str.extract('_(\d)$')
-    mlt.ix[mlt_ps.index, 'var_name'] = mlt_ps['var_name']
+    mlt.ix[ps_base.notnull(), 'variable'] = ps_base + ps_num + ps_rest
+
+    # Extract variable name
+    mlt['var_name'] = mlt.variable.str.extract('([\w_]+)__', expand=False)
+    mlt.ix[mlt.var_name.isnull(), 'var_name'] = mlt.variable
+
+    has_dbl_under = mlt.variable.str.contains('__')
+
+    overall_mlt = mlt.loc[~has_dbl_under].set_index(['i', 'var_name'])['value']
+    overall_stack = overall_mlt.unstack(level=-1)
+    add_net(overall_stack)
+
+    overall_df = overall_stack.reset_index()
+
+    mlt_1_var = mlt.loc[has_dbl_under]
+
+    mlt_1_var['idx_num'] = pd.to_numeric(
+        mlt_1_var.variable.str.extract('__(\d+)'))
+    mlt_1_var[heads[0]['name']] = mlt_1_var.idx_num.map(
+        lambda x: heads[0]['values'][x])
+    mlt_1_var['var_name'] = mlt_1_var.variable.str.extract('([\w_]+)__')
 
     idx_cols = ['i', heads[0]['name'], 'var_name']
-    stacked = mlt.set_index(idx_cols)['value']
+    stacked = mlt_1_var.set_index(idx_cols)['value']
     one_df = stacked.unstack(level=-1)
 
-    # Add net
-    ps_cols = one_df.columns.str.contains('ps\d')
-    all_ps = all_ps = one_df.loc[:, ps_cols].as_matrix()
-    one_df['net'] = all_ps[:, 5:].sum(axis=1) - all_ps[:, :4].sum(axis=1)
+    add_net(one_df)
 
     one_df.reset_index(inplace=True)
 
     return {'overall': overall_df, 1: one_df}
+
+
+def add_net(df):
+    id_vars = df.index.names
+    p_unstk = df.reset_index()
+    p_mlt = pd.melt(p_unstk, id_vars=id_vars)
+    p_rows = p_mlt[p_mlt.var_name.str.contains('ps\d')]
+    p_base = p_rows.var_name.str.extract('(.*)ps\d')
+    p_base_name = p_base.iloc[0]
+    p_rows.set_index(id_vars + ['var_name']).unstack(level=-1)
+    all_ps = p_rows.set_index(
+        id_vars + ['var_name']).unstack(level=-1).as_matrix()
+    net_col = p_base_name + 'net'
+    df[net_col] = all_ps[:, 5:].sum(axis=1) - all_ps[:, :4].sum(axis=1)
--- tests/test_examine_trace.py
+++ tests/test_examine_trace.py
@@ -32,12 +32,13 @@ def regional_trace():
     reg_mu_delta_0 = 0
     reg_mu_delta_1 = 1
     ps = [0.15 for x in range(7)]
-    rec_row = (b0_mu, sigma, reg_mu_delta_0, reg_mu_delta_1, *thresh, *ps, *ps)
+    rec_row = (b0_mu, sigma, reg_mu_delta_0, reg_mu_delta_1, *thresh, *ps, *ps, *ps)
     cols = """b0_mu sigma
     mu_reg__0 mu_reg__1
     thresh__0  thresh__1  thresh__2  thresh__3  thresh__4
-    ps__0_0 ps__0_1 ps__0_2 ps__0_3 ps__0_4 ps__0_5 ps__0_6
-    ps__1_0 ps__1_1 ps__1_2 ps__1_3 ps__1_4 ps__1_5 ps__1_6
+    nat_ps__0 nat_ps__1 nat_ps__2 nat_ps__3 nat_ps__4 nat_ps__5 nat_ps__6
+    reg_ps__0_0 reg_ps__0_1 reg_ps__0_2 reg_ps__0_3 reg_ps__0_4 reg_ps__0_5 reg_ps__0_6
+    reg_ps__1_0 reg_ps__1_1 reg_ps__1_2 reg_ps__1_3 reg_ps__1_4 reg_ps__1_5 reg_ps__1_6
     """.split()
 
     trace_df = pd.DataFrame.from_records(
@@ -50,7 +51,14 @@ def regional_trace():
 
 def test_overall_metrtics_df(regional_trace):
     res = label_trace(regional_trace['trace'], regional_trace['heads'])
-    assert {'b0_mu', 'sigma', 'i', 'thresh__3'} <= set(res['overall'].columns)
+    oall = res['overall']
+    assert {'b0_mu', 'sigma', 'i', 'thresh3', 'nat_ps0'} <= set(oall.columns)
+
+
+def test_overall_metrtics_net(regional_trace):
+    res = label_trace(regional_trace['trace'], regional_trace['heads'])
+    oall = res['overall']
+    assert (oall.loc[:, 'nat_net'] == -0.3).all()
 
 
 def test_reg_metrtics_df(regional_trace):
@@ -58,9 +66,10 @@ def test_reg_metrtics_df(regional_trace):
     reg_df = res[1]
     assert (reg_df.loc[reg_df.Region == 'Alabama', 'mu_reg'] == 0).all()
     assert (reg_df.loc[reg_df.Region == 'Atlanta', 'mu_reg'] == 1).all()
-    assert (reg_df.loc[reg_df.Region == 'Atlanta', 'ps0'] == 0.15).all()
+    assert (reg_df.loc[reg_df.Region == 'Atlanta', 'reg_ps0'] == 0.15).all()
+
 
 def test_reg_nets_df(regional_trace):
     res = label_trace(regional_trace['trace'], regional_trace['heads'])
     reg_df = res[1]
-    assert (reg_df.loc[reg_df.Region == 'Alabama', 'net'] == -0.3).all()
+    assert (reg_df.loc[reg_df.Region == 'Alabama', 'reg_net'] == -0.3).all()
