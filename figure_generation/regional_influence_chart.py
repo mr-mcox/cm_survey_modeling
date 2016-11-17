a36ec5d2742a28fd922cd3155dec3cd2e85a11c1
Matthew Cox <matthew.cox@teachforamerica.org>
2016-11-17 12:21:25 -0600

    Adding national to regional influence chart

--- figure_generation/regional_influence_chart.py
+++ figure_generation/regional_influence_chart.py
@@ -46,18 +46,29 @@ with pd.HDFStore(path.join('..', 'inputs', 'processed', 'reg_influence.h5')) as 
         
         store['overall'] = oall
         store['region'] = regs
+        
+nat_csi = oall.groupby('i')['nat_net'].mean().reset_index()
+nat_csi_qs = nat_csi.loc[:,'nat_net'].quantile([0.05, 0.25, 0.75, 0.95])
+b_width_l = nat_csi_qs[0.95] - nat_csi_qs[0.05]
+b_width_s = nat_csi_qs[0.75] - nat_csi_qs[0.25]
     
-reg_csi = regs.groupby(['Region','i'])['net'].mean().reset_index()
+reg_csi = regs.groupby(['Region','i'])['reg_net'].mean().reset_index()
 
-reg_csi_qs = reg_csi.groupby('Region')['net'].quantile([0.05, 0.25, 0.75, 0.95]).unstack(level=-1)
+reg_csi_qs = reg_csi.groupby('Region')['reg_net'].quantile([0.05, 0.25, 0.75, 0.95]).unstack(level=-1)
 reg_csi_qs.sort_values(by=0.05, inplace=True)
 reg_csi_qs['b_width_l'] = reg_csi_qs[0.95] - reg_csi_qs[0.05]
 reg_csi_qs['b_width_s'] = reg_csi_qs[0.75] - reg_csi_qs[0.25]
 
 fig, ax = plt.subplots(figsize=(10, 15))
 fig.suptitle('CSI at 1617F8W by Region')
-plt.barh(bottom=np.arange(len(reg_csi_qs))-0.5, width=reg_csi_qs.b_width_l, left=reg_csi_qs[0.05], alpha=0.4, label='90% credible CSI')
-plt.barh(bottom=np.arange(len(reg_csi_qs))-0.5, width=reg_csi_qs.b_width_s, left=reg_csi_qs[0.25], alpha=0.2)
+
+colors = sns.color_palette("Greys_r")
+plt.barh(bottom=-0.5, width=b_width_l, left=nat_csi_qs[0.05], height=len(reg_csi_qs) + 0.5, color=colors[4], linewidth=0, label='90% credible National CSI')
+plt.barh(bottom=-0.5, width=b_width_s, left=nat_csi_qs[0.25], height=len(reg_csi_qs) + 0.5, color=colors[3], linewidth=0, label='50% credible National CSI')
+
+colors = sns.color_palette("Blues_r")
+plt.barh(bottom=np.arange(len(reg_csi_qs))-0.5, width=reg_csi_qs.b_width_l, left=reg_csi_qs[0.05], color=colors[2], linewidth=0, label='90% credible CSI')
+plt.barh(bottom=np.arange(len(reg_csi_qs))-0.5, width=reg_csi_qs.b_width_s, left=reg_csi_qs[0.25], color=colors[1], linewidth=0, label='50% credible CSI')
 plt.ylim(ymin=-0.5, ymax=len(reg_csi_qs))
 ax.yaxis.set_ticks(range(len(reg_csi_qs)))
 ax.set_yticklabels(reg_csi_qs.index)
@@ -65,4 +76,5 @@ xvals = ax.get_yticks()
 ax.set_xticklabels(['{:3.0f}%'.format(x*10) for x in xvals])
 ax.legend(loc='lower right')
 plt.show()
+
 
